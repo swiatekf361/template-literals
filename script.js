b4822b427ecd634cea15b77a031acba7776a6e08
Fabian Świątek <144426209+swiatekf361@users.noreply.github.com>
2025-12-25 19:55:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function convert() {
+    src = document.getElementById("es6").value;
+    literals = src.match(/`[^`]*`/g);
+    for (l in literals) {
+        original_literal = literals[l];
+            literals[l] = literals[l].replaceAll("\"", "\\\"")
+        invars = literals[l].match(/\$\{[^{^}]+\}/g);
+        literals[l] = literals[l].replaceAll("`", '"');
+
+        for (i in invars)
+			literals[l] = literals[l].replace(invars[i], '" + ' + invars[i].slice(2, -1) + ' + "');
+        
+        
+        src = src.replace(original_literal, literals[l]);
+		src = src.replace(' + ""', "");
+		src = src.replace(" + ''", "");
+    }
+    res = document.getElementById("old");
+    res.value = src;
+	// document.getElementById("copy").disabled = false;
+}
+
+/*function copytxt() {
+	navigator.clipboard.writeText(document.getElementById("old").value);
+}*/
